c0a84a9f4f13a4b4d22521605f03f5f0efd8ed4a
Hararec MG <hararecmedina@gmail.com>
2023-03-26 17:30:21 -0600

    chore: se agregan modificaciones para usarlas como notas

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 require('dotenv').config();
+const { application } = require('express');
 const Server = require('./models/server');
 
 const server = new Server();
 server.listen();
+
+// app.get('/generic', (req, res) => {
+//     res.sendFile(path.join(__dirname, '/public/generic.html'));
+// });
